6919810488e3b5bad341e978a6df0e25eb4bc75c
Braiton57 <143307681+Braiton57@users.noreply.github.com>
2023-11-10 21:32:43 +0000

    generador de nombres

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let c = 0; c < adj.length; c++) {
+      for (let n = 0; n < noun.length; n++) {
+        console.log(pronoun[i] + adj[c] + noun[n] + ".com");
+      }
+    }
+  }
 };
